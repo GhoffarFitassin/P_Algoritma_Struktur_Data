6f3ec32bdff78e5e84cce7cde8235967fb008329
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2024-03-18 22:35:38 +0700

    add Percobaan 1 Faktorial

---
+++ jobsheet4/src/BruteForceDivideConquer/Faktorial.java
@@ -0,0 +1,31 @@
+package BruteForceDivideConquer;
+
+public class Faktorial {
+    public int nilai;
+
+    public Faktorial(){
+
+    }
+
+    public Faktorial(int nilai){
+        this.nilai=nilai;
+    }
+    int faktorialBF(int n){
+        int fakto = 1;
+        for (int i = 1; i <= n; i++) {
+            fakto = fakto * i;
+        }
+        return fakto;
+    }
+
+    int faktorialDC(int n){
+        if (n == 1) {
+            return 1;
+        } else {
+            int fakto = n * faktorialDC(n-1);
+            return fakto;
+        }
+    }
+
+    
+}
