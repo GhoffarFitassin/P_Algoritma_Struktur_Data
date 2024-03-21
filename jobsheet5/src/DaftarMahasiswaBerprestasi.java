1cf0f9ee21c775afc8bf8b7fe9f6aaf9b7bdacd3
ghoffar8804@gmail.com <ghoffar8804@gmail.com>
2024-03-21 23:48:53 +0700

    feat: percobaan 2

--- jobsheet5/src/DaftarMahasiswaBerprestasi.java
+++ jobsheet5/src/DaftarMahasiswaBerprestasi.java
@@ -22,11 +22,27 @@ public class DaftarMahasiswaBerprestasi {
         for (int i = 0; i < listMhs.length-1; i++) {
             for (int j = 1; j < listMhs.length-i; j++) {
                 if (listMhs[j].ipk > listMhs[j-1].ipk) {
+                    // swap
                     Mahasiswa tmp = listMhs[j];
                     listMhs[j] = listMhs[j-1];
                     listMhs[j-1] = tmp;
                 }
             }
+        }
+    }
+
+    void selectionSort(){
+        for (int i = 0; i < listMhs.length-1; i++) {
+            int idxMin = i;
+            for (int j = i+1; j < listMhs.length; j++) {
+                if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                    idxMin = j;
+                }
+            }
+            // swap
+            Mahasiswa tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
         }
     }
 }
