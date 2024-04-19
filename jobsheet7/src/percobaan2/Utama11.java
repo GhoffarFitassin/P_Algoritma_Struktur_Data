e916bef3c9dd7307597d47ce67cfee4fe1db6d9e
GhoffarFitassin <ghoffar8804@gmail.com>
2024-04-19 21:40:42 +0700

    add percobaan 2 jobsheet7

---
+++ jobsheet7/src/percobaan2/Utama11.java
@@ -0,0 +1,52 @@
+package percobaan2;
+import java.util.Scanner;
+
+public class Utama11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Kapasitas: ");
+        int cap = scanner.nextInt();
+        Gudang11 gudang = new Gudang11(cap);
+
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Tambah barang");
+            System.out.println("2. Ambil barang");
+            System.out.println("3. Tampilkan tumpukan barang");
+            System.out.println("4. Lihat barang teratas");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih operasi: ");
+            int pilihan = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan kode barang: ");
+                    int kode = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Masukkan nama barang: ");
+                    String nama = scanner.nextLine();
+                    System.out.print("Masukkan nama kategori: ");
+                    String kategori = scanner.nextLine();
+                    Barang11 barangBaru = new Barang11(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+
+                case 2:
+                    gudang.ambilBarang();
+                    break;
+                case 3:
+                    gudang.tampilkanBarang();
+                    break;
+                case 4:
+                    gudang.lihatBarangTeratas();
+                    break;
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih lagi");
+            }
+        }
+    }
+}
