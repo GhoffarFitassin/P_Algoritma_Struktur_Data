2d501601a03bc4da0bda9a67b664ffba3c69f225
GhoffarFitassin <ghoffar8804@gmail.com>
2024-05-21 21:19:08 +0700

    add praktikum 1, 2, 3

---
+++ jobsheet10/src/praktikum2/DoubleLinkedListsMain.java
@@ -0,0 +1,26 @@
+package praktikum2;
+
+public class DoubleLinkedListsMain {
+    public static void main(String[] args) {
+        DoubleLinkedLists dll = new DoubleLinkedLists();
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("==========================");
+    }
+}
---
+++ jobsheet10/src/praktikum2/Node.java
@@ -0,0 +1,12 @@
+package praktikum2;
+
+public class Node {
+    int data;
+    Node prev, next;
+
+    Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
